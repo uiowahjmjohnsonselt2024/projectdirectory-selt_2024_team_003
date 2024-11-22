b3675214d89eeae57a650ad151a97a2a41fbbece
Nick Hageman <nickhageman8@gmail.com>
2024-11-21 18:43:15 -0600

    Began creating new page for our in-game shop

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post 'signup', to: 'registrations#create'
   get 'pages/grid'
   get 'grid', to: 'pages#grid'  # This defines the route for /grid
+  get 'store', to: 'store#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
