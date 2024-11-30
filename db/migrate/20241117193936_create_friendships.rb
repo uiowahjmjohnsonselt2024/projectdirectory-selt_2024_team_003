a5e6a849f4d349858fd3bb9eef40f62066a08492
Nate Schaefer <nathanschaefer14@gmail.com>
2024-11-29 19:55:53 -0600

    changing friendship migration

--- db/migrate/20241117193936_create_friendships.rb
+++ db/migrate/20241117193936_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[8.0]
   def change
     create_table :friendships do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+      t.references :user, { to_table: :users }, index: true
+      t.references :friend, { to_table: :users }, index: true
 
       t.timestamps null: false
     end
