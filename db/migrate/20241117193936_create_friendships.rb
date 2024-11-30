6f275eb9e4aaca6dc45556cefbfdd71f13b2889d
Nate Schaefer <nathanschaefer14@gmail.com>
2024-11-29 20:33:34 -0600

    test commit for friend table error

--- db/migrate/20241117193936_create_friendships.rb
+++ db/migrate/20241117193936_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[8.0]
   def change
     create_table :friendships do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: { to_table: :users }
+      t.references :friend, index: true, foreign_key: { to_table: :users }
 
       t.timestamps null: false
     end
