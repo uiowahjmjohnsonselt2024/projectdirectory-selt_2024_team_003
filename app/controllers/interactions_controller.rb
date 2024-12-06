ed3ec81afcd047d50d80c4530dccc7b8c2afa0b7
Nick Hageman <nickhageman8@gmail.com>
2024-12-06 11:45:01 -0600

    Modifying battling interface

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -4,6 +4,16 @@ class InteractionsController < ApplicationController
     @game_user = @game.game_users.find_by(user: current_user)
     @enemy = @game.enemies.find_by(x_position: @game_user.x_position, y_position: @game_user.y_position)
     @player = current_user
+    case current_user.archetype
+    when 'Arcane Strategist'
+      @image = 'attack.png'
+    when 'Iron Guardian'
+      @image = 'defense.png'
+    when 'Omni Knight'
+      @image = 'balanced.png'
+    else
+      @image = 'balanced.png'
+    end
   end
 
   def attack
