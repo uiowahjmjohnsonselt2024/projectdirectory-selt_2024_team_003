857756443ed5012c3106ac9ca9e05ca8f436e83f
Nick Hageman <nickhageman8@gmail.com>
2024-12-11 12:53:06 -0600

    SimpleCov update

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   gem 'shoulda-matchers'
   # gem 'capybara'
   # gem 'selenium-webdriver'
+  gem 'simplecov', require: false
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,12 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/spec/' # Exclude test files from coverage
+  add_filter '/config/' # Optionally exclude config files
+end
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
