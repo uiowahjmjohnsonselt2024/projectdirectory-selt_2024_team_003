fbf7b677583eceb73d67cf9d0c2311d81518d6ce
Eli Paulsen <eli-paulsen@uiowa.edu>
2024-12-11 21:42:27 -0600

    error messages fix in registration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_10_014455) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_12_000259) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -84,8 +83,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_10_014455) do
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.string "code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -110,6 +109,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_10_014455) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
     t.integer "health", default: 100
     t.integer "attack", default: 10
     t.integer "defense", default: 5
@@ -121,11 +122,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_10_014455) do
     t.integer "special_attack"
     t.integer "special_defense"
     t.integer "shards", default: 0, null: false
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -133,8 +129,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_10_014455) do
   add_foreign_key "enemies", "games"
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
-  add_foreign_key "game_users", "games"
-  add_foreign_key "game_users", "users"
   add_foreign_key "messages", "users"
   add_foreign_key "skins", "users"
 end
