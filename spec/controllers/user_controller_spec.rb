643b240b265cdc425cbc0641cf66a900b76c22fe
Eli Paulsen <eli-paulsen@uiowa.edu>
2024-12-13 22:01:02 -0600

    missing rspec files added back in

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+describe 'UserController' do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      skip 'Not implemented'
+    end
+  end
+end
