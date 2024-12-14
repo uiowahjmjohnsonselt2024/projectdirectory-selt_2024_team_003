813232c7e6a457d8694de23973ebb1ba6df29d34
Shaun Richter <shaunrichter547@gmail.com>
2024-12-13 22:22:53 -0600

    Fixed casino bug issues, and added direct acess back to casino page from store

--- app/controllers/casino_controller.rb
+++ app/controllers/casino_controller.rb
@@ -12,39 +12,24 @@ class CasinoController < ApplicationController
     winner = params[:winner] # The winner ('red', 'black', or 'green') passed from the frontend
 
     # Check if the user has enough shards to place the bet
-    if bet_amount >= 0 && current_user.shards >= bet_amount
-      # Deduct the bet amount from the user's shards
+    if bet_amount > 0 && current_user.shards >= bet_amount
       if winner == "pending"
-        current_user.shards -= bet_amount
-      end
-
-      if current_user.save
-        # If winner is set, calculate win/loss after spin
-        if winner != "pending"
-          is_win = user_bet == winner
-          if is_win
-            winnings = bet_amount * 2
-            current_user.shards += winnings
-          end
-
-          # Save updated shards
-          if current_user.save
-            result = is_win ? "You won! #{winnings} shards." : "You lost! The spin landed on #{winner}. Better luck next time."
-            render json: { success: true, result: result, shards: current_user.shards, is_win: is_win }
-          else
-            render json: { success: false, message: 'Failed to update shards.' }, status: :unprocessable_entity
-          end
-        else
-
-          render json: { success: true, message: 'Bet placed successfully.' }
-        end
+        render json: { success: true, message: 'Bet placed successfully.' }
       else
-        render json: { success: false, message: 'Failed to deduct shards.' }, status: :unprocessable_entity
+        is_win = user_bet == winner
+        winnings = is_win ? bet_amount * (winner == "green" ? 14 : 2) : 0
+        current_user.shards += winnings - bet_amount
+
+        # Save updated shards
+        if current_user.save
+          result = is_win ? "You won! #{winnings} shards." : "You lost! The spin landed on #{winner}. Better luck next time."
+          render json: { success: true, result: result, shards: current_user.shards, is_win: is_win }
+        else
+          render json: { success: false, message: 'Failed to update shards.' }, status: :unprocessable_entity
+        end
       end
     else
-      if winner == "pending"
-        render json: { success: false, message: 'Insufficient shards or invalid bet amount.' }, status: :unprocessable_entity
-      end
+      render json: { success: false, message: 'Insufficient shards or invalid bet amount.' }, status: :unprocessable_entity
     end
   end
 
