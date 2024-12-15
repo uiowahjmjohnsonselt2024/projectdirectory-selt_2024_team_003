07fa7dd437c232e5881dca3c0a427e457de57f47
Nick Hageman <nickhageman8@gmail.com>
2024-12-14 18:10:19 -0600

    Added sounds, animations, consumables into gameplay

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -61,11 +61,10 @@ class InteractionsController < ApplicationController
       @player.update(experience: @player.experience + experience_gain)
       level_up_if_needed(@player)
       message += " You defeated the enemy and gained #{experience_gain} XP!"
-
-      @player.add_achievement("First Blood: The thrill of victory, the first taste of triumph.")
+      current_user.add_achievement("First Blood: The thrill of victory, the first taste of triumph.")
 
       if @enemy.x_position == 5 && @enemy.y_position == 5
-        @player.add_achievement("The Final Boss: Defeat the final boss and claim victory over the game.")
+        current_user.add_achievement("The Final Boss: Defeat the final boss and claim victory over the game.")
         render json: { success: true, win: true, message: message } and return
       end
     else
@@ -101,6 +100,13 @@ class InteractionsController < ApplicationController
 
   def use_consumable
     @game = Game.find_by(code: params[:game_id])
+    @game_user = @game.game_users.find_by(user: current_user)
+    @enemy = @game.enemies.find_by(x_position: @game_user.x_position, y_position: @game_user.y_position)
+    @player = current_user.current_skin # Use current_skin for player stats
+    @game_id = params[:game_id]
+    message = ''
+    player_defeated = false
+
     if @game.nil?
       render json: { success: false, message: 'Game not found!' }, status: :not_found and return
     end
@@ -133,11 +139,62 @@ class InteractionsController < ApplicationController
         consumable_quantity: consumable.quantity,
         player_health: max_health,
         player_mana: @game_user.mana,
+        enemy_health: @enemy.health,
+        enemy_max_health: @enemy.max_health,
+        max_player_health: @player.health,
+        player_defeated: player_defeated
       }
     elsif consumable.name == 'Acid Potion'
-      Rails.logger.debug("Acid placeholder")
+      @enemy.update(health: @enemy.health - 100)
+
+      # Decrease consumable quantity
+      consumable.decrement!(:quantity)
+      if consumable.quantity.zero?
+        consumable.destroy
+      end
+      if @enemy.health <= 0
+        @enemy.update(health: 0)
+        experience_gain = @enemy.level * 50
+        @player.update(experience: @player.experience + experience_gain)
+        level_up_if_needed(@player)
+        message += " You defeated the enemy and gained #{experience_gain} XP!"
+        current_user.add_achievement("First Blood: The thrill of victory, the first taste of triumph.")
+
+        if @enemy.x_position == 5 && @enemy.y_position == 5
+          current_user.add_achievement("The Final Boss: Defeat the final boss and claim victory over the game.")
+          render json: { success: true, win: true, message: message } and return
+        end
+      else
+        enemy_damage, enemy_critical = calculate_attack_damage(@enemy, @player)
+        @game_user.update(health: @game_user.health - enemy_damage)
+
+        message += if enemy_critical
+                     " Critical Hit! The enemy dealt #{enemy_damage} damage to you!"
+                   else
+                     " The enemy attacked you and dealt #{enemy_damage} damage."
+                   end
+      end
+
+      if @game_user.health <= 0
+        @game_user.update(health: @player.health)
+        @game_user.update(mana: @player.mana)
+        @game_user.update(x_position: 0, y_position: 0)
+        message += ' You have been defeated and sent back to the starting position!'
+        player_defeated = true
+      end
+
+      render json: {
+        success: true,
+        message: message,
+        consumable_quantity: consumable.quantity,
+        player_health: @game_user.health,
+        player_mana: @game_user.health,
+        enemy_health: @enemy.health,
+        enemy_max_health: @enemy.max_health,
+        max_player_health: @player.health,
+        player_defeated: player_defeated
+      }
     elsif consumable.name == 'Mana Refill'
-      Rails.logger.debug("Mana placeholder")
       max_mana = current_user.current_skin.mana
       @game_user.update(mana: max_mana)
 
@@ -152,10 +209,30 @@ class InteractionsController < ApplicationController
         consumable_quantity: consumable.quantity,
         player_health: @game_user.health,
         player_mana: max_mana,
+        enemy_health: @enemy.health,
+        enemy_max_health: @enemy.max_health,
+        max_player_health: @player.health,
+        player_defeated: player_defeated
       }
-
     elsif consumable.name == 'Health Potion'
-      Rails.logger.debug("Health Potion placeholder")
+      @game_user.update(health: @game_user.health + 100)
+
+      # Decrease consumable quantity
+      consumable.decrement!(:quantity)
+      if consumable.quantity.zero?
+        consumable.destroy
+      end
+      render json: {
+        success: true,
+        message: "You used #{consumable.name}! Health restored to #{@game_user.health}.",
+        consumable_quantity: consumable.quantity,
+        player_health: @game_user.health,
+        player_mana: @game_user.mana,
+        enemy_health: @enemy.health,
+        enemy_max_health: @enemy.max_health,
+        max_player_health: @player.health,
+        player_defeated: player_defeated
+      }
     else
       render json: { success: false, message: 'Consumable has no effect!' }
     end
@@ -169,7 +246,7 @@ class InteractionsController < ApplicationController
     @game_id = params[:game_id]
     message = ''
 
-    case current_user.archetype
+    case current_user.current_skin.archetype
     when 'Attacker'
       if @game_user.use_mana(20)
         if rand(1..100) <= 10
@@ -309,12 +386,12 @@ class InteractionsController < ApplicationController
     # Define the multipliers for each weapon
     # ["Sword", "Flame Sword", "Bow and Arrow", "Shotgun", "Sniper"]
     weapon_multipliers = {
-      'knife' => 1.0,
-      'sword' => 1.5,
-      'flame sword' => 2.0,
-      'bow and arrow' => 2.5,
-      'shotgun' => 5.0,
-      'sniper' => 10.0
+      'knife' => 1,
+      'sword' => 2,
+      'flame sword' => 4,
+      'bow and arrow' => 8,
+      'shotgun' => 16,
+      'sniper' => 32
     }
 
     # Return the multiplier for the weapon, default to 1 if weapon is not found
